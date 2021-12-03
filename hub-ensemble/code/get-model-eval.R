d79cbf1e3c4c5a78eb64e3b49846672b5037bedb
kathsherratt <kathsherratt@gmail.com>
2021-12-03 00:15:25 +0000

    update results

--- hub-ensemble/code/get-model-eval.R
+++ hub-ensemble/code/get-model-eval.R
@@ -1,5 +1,5 @@
-subproj_dir <- "research/intro-paper"
-here::i_am(paste0(subproj_dir, "/R/get-model-eval.R"))
+subproj_dir <- "hub-ensemble"
+here::i_am(paste0(subproj_dir, "/code/get-model-eval.R"))
 
 # Get evaluation dataset
 library(here)
@@ -15,16 +15,15 @@ library(covidHubUtils)
 eval_date <- dir(here("covid19-forecast-hub-europe", "evaluation", "weekly-summary"))
 eval_date <- sort(as.Date(gsub("(evaluation-)|(\\.csv)", "", eval_date)))
 eval_date <- eval_date[length(eval_date)]
-eval_file <- here("covid19-forecast-hub-europe", "evaluation", "weekly-summary", paste0("evaluation-", eval_date, ".csv"))
+eval_file <- here("covid19-forecast-hub-europe", "evaluation", "weekly-summary",
+                  paste0("evaluation-", eval_date, ".csv"))
 
 # clean variable names
 clean_variables <- c("inc case" = "Cases", "inc death" = "Deaths")
 
-# get model designations - in order to remove "other" except baseline
-model_desig <- covidHubUtils::get_model_designations(source = "local_hub_repo",
-                                                     hub_repo_path = here("covid19-forecast-hub-europe")) %>%
-  mutate(designation = case_when(model == "EuroCOVIDhub-baseline" ~ "secondary",
-                                 TRUE ~ designation))
+# get model designations - in order to remove "other"
+model_desig <- get_model_designations(source = "local_hub_repo",
+                                      hub_repo_path = here("covid19-forecast-hub-europe"))
 
 # clean eval dataset ------------------------------------------------------
 
@@ -33,42 +32,43 @@ model_eval <- read_csv(eval_file) %>%
   # keep only 1-4 horizons
   filter(horizon <= 4) %>%
   # clean up team-model names
-  separate(model, into = c("team_name", "model_name"), 
-           sep = "-", remove = FALSE) %>%  
+  separate(model, into = c("team_name", "model_name"),
+           sep = "-", remove = FALSE) %>%
   mutate(
-    # add neat variables, esp useful for plots
+    # add neat variables useful for plots
     target_variable = recode(target_variable, !!!clean_variables),
+    model = factor(model, ordered = TRUE),
     # ensure scores are numeric
-    across(c(horizon, rel_ae:n_loc), as.numeric)) %>%
-  
-  filter() %>%
-  mutate(model = factor(model, ordered = TRUE)) %>%
-  # Remove from evaluation:
+    across(c(horizon, rel_ae:n_loc), as.numeric),
+    # Set Inf value to NA
+    across(rel_ae:bias, ~ na_if(., Inf)))
+
+# Exclude some models and targets from evaluation:
+model_eval <- model_eval %>%
   filter(
     # hospitalisations
     target_variable %in% clean_variables &
+    # location summary
+    location != "Overall" &
     # models designated "other"
     !model %in% filter(model_desig, designation == "other")$model &
+    # hub baseline (should already be excluded by "other" designation)
+    !grepl("hub-baseline", model) &
     # 10 week evaluations (keep only evaluation based on all history)
     weeks_included == "All")
 
-
-# tidy up -----------------------------------------------------------------
-# separate out ensemble as comparator
+# separate out ensemble scores into own column
 score_ensemble <- model_eval %>%
   filter(grepl("hub-ensemble", model)) %>%
-  select(ensemble_rel_wis = rel_wis,
-         ensemble_rel_ae = rel_ae,
-         weeks_included, target_variable, horizon, location) %>%
-  mutate(baseline_rel_wis = 1,
-         baseline_rel_ae = 1)
+  select(weeks_included, target_variable, horizon, location,
+         ensemble_rel_wis = rel_wis,
+         ensemble_rel_ae = rel_ae)
 
-model_eval_wide <- model_eval %>%
-  filter(!grepl("hub-baseline", model)) %>% # leave ensemble as row as well as col
-  full_join(score_ensemble)
-
-fig_date <- paste("Evaluation as at", eval_date)
+# add ensemble to every row; leaves ensemble as row as well as col
+model_eval <- model_eval %>%
+  full_join(score_ensemble,
+            by = c("weeks_included", "target_variable", "horizon", "location")) %>%
+  mutate(is_hub = grepl("hub-ensemble", model))
 
 ###
 rm(model_desig, clean_variables, score_ensemble)
-   
